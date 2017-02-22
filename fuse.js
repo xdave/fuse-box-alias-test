0c647f2bcc8f707dabe483f18b6c69a2ccdef058
Dave Gradwell <deft.productions@gmail.com>
2017-02-21 19:30:02 -0700

    Initial commit.

---
+++ fuse.js
@@ -0,0 +1,46 @@
+const fs = require('fs');
+const path = require('path');
+const fsbx = require('fuse-box');
+
+const homeDir = path.resolve('.', 'src');
+const publicDir = path.resolve('.', 'public');
+const name = 'app';
+const outFile = path.join(publicDir, name + '.js');
+const html = 'index.html';
+const entry = '>index.tsx';
+const prod = process.env.NODE_ENV === 'production';
+
+const config = {
+    alias: {
+        'react': 'preact-compat',
+        'react-dom': 'preact-compat'
+    },
+    homeDir,
+    outFile,
+    sourceMap: {
+        bundleReference: path.basename(outFile) + '.map',
+        outFile: outFile + '.map'
+    },
+    plugins: [
+        {
+            init(context) {
+                try { fs.mkdirSync(publicDir); } catch (err) { }
+            },
+            bundleEnd(context) {
+                fs.createReadStream(path.join(homeDir, html))
+                    .pipe(fs.createWriteStream(path.join(publicDir, html)));
+            }
+        },
+        fsbx.JSONPlugin(),
+        fsbx.EnvPlugin({ NODE_ENV: process.env.NODE_ENV }),
+        prod ? fsbx.UglifyJSPlugin() : {},
+    ]
+};
+
+const fuse = new fsbx.FuseBox(config);
+
+if (process.argv.indexOf('--dev') > -1) {
+    fuse.devServer(entry);
+} else {
+    fuse.bundle(entry);
+}
---
+++ src/index.tsx
@@ -0,0 +1,74 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import { Provider, connect } from 'react-redux';
+
+import { Action, ActionCreator, Dispatch, Store } from 'redux';
+import { bindActionCreators, createStore } from 'redux';
+
+interface State {
+    message: string;
+}
+
+interface SomeAction extends Action {
+    type: 'SOME_ACTION';
+    message: string;
+}
+
+interface Actions {
+    someAction: ActionCreator<SomeAction>;
+}
+
+const someAction = (message: string): SomeAction => {
+    return {
+        type: 'SOME_ACTION',
+        message
+    };
+};
+
+const initial: State = {
+    message: 'This is the initial state'
+};
+
+const someReducer = (state: State = initial, action: SomeAction) => {
+    switch (action.type) {
+        case 'SOME_ACTION':
+            return {
+                ...state,
+                message: action.message
+            };
+        default:
+            return state;
+    }
+};
+
+const MainView = ({ message, ...actions }: State & Actions) => (
+    <div>
+        <h1>Message: {message}</h1>
+        <button onClick={e => actions.someAction('Hello, World!')}>
+            Change Message
+        </button>
+    </div>
+);
+
+const mapStateToProps = (state: State) => state;
+const mapDispatchToProps = (dispatch: Dispatch<State>) => {
+    return bindActionCreators({ someAction }, dispatch);
+};
+
+const Main = connect(mapStateToProps, mapDispatchToProps)(MainView);
+
+
+const win: any = window;
+win.__store = win.__store || createStore(someReducer);
+const element = document.querySelector('#app');
+
+if (element !== null) {
+    element.innerHTML = '';
+
+    ReactDOM.render((
+        <Provider store={win.__store}>
+            <Main />
+        </Provider>
+    ), element);
+}
+
